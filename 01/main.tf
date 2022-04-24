b86f65bdf0c041bb7ad297c869c55c200fd9990d
Jeongjiho <claraster84@gmail.com>
2022-04-24 17:44:16 +0900

    init

---
+++ 01/main.tf
@@ -0,0 +1,22 @@
+provider "google" {
+  credentials = file("key.json")
+  project = "terraform-348208"
+  region = "asia-northeast3"
+}
+
+resource "google_compute_instance" "example" {
+  name = "example"
+  machine_type = "f1-micro"
+  zone = "asia-northeast3-a"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-10-buster-v20220406"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {}
+  }
+}
