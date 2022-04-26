c29a2479eb4014f577c454880db2abd54456d2fc
Jeongjiho <claraster84@gmail.com>
2022-04-26 09:46:51 +0900

    commit

---
+++ 04/main.tf
@@ -0,0 +1,43 @@
+provider "google" {
+  credentials = file("key.json")
+  project = "terraform-348208"
+  region = "asia-northeast3"
+}
+
+resource "google_compute_instance" "example" {
+  name = "webserver"
+  machine_type = "f1-micro"
+  zone = "asia-northeast3-a"
+
+  boot_disk {
+    initialize_params {
+      image = "gcr.io/google-containers/busybox"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {}
+  }
+
+  tags = ["web"]
+
+  metadata_startup_script = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p ${var.server_port} &
+              EOF
+}
+
+resource "google_compute_firewall" "default" {
+  name    = "webserver-firewall"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports    = [var.server_port]
+  }
+
+  source_tags = ["web"]
+  target_tags = ["web"]
+}
